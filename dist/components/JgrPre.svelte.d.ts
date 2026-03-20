00f84b7599a02dd8caeb36dd3f10b156f784ec60
JGrimbert <jean.grimbert@gmail.com>
2026-03-20 03:18:33 +0100

    feat: #55 gère pre

---
+++ dist/components/JgrPre.svelte.d.ts
@@ -0,0 +1,8 @@
+type $$ComponentProps = {
+    content: string;
+    previewLines?: number;
+    maxHeight?: string;
+};
+declare const JgrPre: import("svelte").Component<$$ComponentProps, {}, "">;
+type JgrPre = ReturnType<typeof JgrPre>;
+export default JgrPre;
